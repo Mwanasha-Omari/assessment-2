9c78d1f69f26ec6440e9898b2f74edee65933a63
Ali-Mwanasha <alimwanashaomari@gmail.com>
2024-03-25 08:24:14 +0300

    created functions

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+fun main() {
+println(statement("Barnie bakes brown bagels and buns"))
+    (isNumber(20,30,40))
+    (volume(3.14159))
+    (isPalindrome("Kenya"))
+}
+
+fun statement( word:String){
+     word.length
+
+
+
+}
+
+fun isNumber(a:Int,b:Int,c:Int,):Int{
+    var numbers= (a+b+c)
+
+    return numbers
+
+}
+fun volume(number:Number ):Int{
+
+
+}
+
+
+fun isPalindrome(word:String):Boolean{
+   if(){
+
+           return true
+       }
+
+    else {
+     return false
+   }}
+
